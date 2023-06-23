94784e7633e6bb9f63f0270bf604d4db0672c927
stefanodigregoli <stefano.digregoli2001@gmail.com>
2023-06-23 14:42:36 +0200

    Constructor Create a class called House that has 3 instance variables: a string address an int numberOfFloors Define a constructor method for House that will print in console a message as soon as a House object is created Define a testing class (e.g. Tester, Start or another reasonable name you prefer) with a main method where: you declare two House variables house1 and house2 you create two House objects and assign them to the variables house1 and house2

---
+++ src/House.java
@@ -0,0 +1,12 @@
+public class House {
+    String address;
+    int numberofFloor;
+
+    House(String address,int numberofFloor){
+        this.address = address;
+        this.numberofFloor = numberofFloor;
+        System.out.println("Ho creato una nuova casa " + address + " con piani :" + numberofFloor);
+
+    }
+
+}
---
+++ src/Tester.java
@@ -0,0 +1,7 @@
+public class Tester {
+    public static void main(String[] args) {
+        House house1 = new House("via roma 52" , 2);
+        House house2 = new House(" via ariosto 22" , 2);
+    }
+
+}
